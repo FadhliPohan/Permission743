4a3ed8e38bd157193d763a5f99643bcd6d947db9
FadhliPohan <muhammadfadly.mfd@gmail.com>
2023-08-01 08:39:42 +0700

    yazra

--- app/Http/Controllers/user/PermissionController.php
+++ app/Http/Controllers/user/PermissionController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Spatie\Permission\Models\Permission;
 
-use Yajra\DataTables\DataTables;
 
 class PermissionController extends Controller
 {
